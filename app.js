0905916851134a6b34e9093c3ec75841895f723f
augustinottoumbazoa <163024749+augustinottoumbazoa@users.noreply.github.com>
2024-04-14 23:58:03 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,157 @@
-// Création d'un tableau pour stocker les articles
-let articles = [];
+let save = document.querySelector(".btn-save")
+let inputtile= document.getElementById("titlefield")
+let contain= document.querySelector(".cards-grid")
+let inputext= document.getElementById("description")
+let i =0
+let compt =0
+let look = 0
 
-// Fonction pour ajouter un article à la liste
-function ajouterArticle() {
-    // Récupération des valeurs des champs d'entrée
-    let titre = document.getElementById('titre').value;
-    let description = document.getElementById('description').value;
+let isUpdate = false, updateId;
 
-    // Création d'un nouvel objet article
-    let article = {
-        titre: titre,
-        description: description
-    };
 
-    // Ajout de l'article à la liste
-    articles.push(article);
+function addCard(i ,inputtile,inputext){
 
-    // Affichage de l'article dans la console
-    console.log("Article ajouté : ", article);
+ // cree la div qui  a pour class card
+ let card = document.createElement("div")
+ card.className="card"// donne la class card a la div cree
+ contain.appendChild(card)// insere ce bloc dans la balise qui a pour class .cards-grid appeler contain
+ /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+ // cree le div qui  a pour class card-actions
+ let card_action = document.createElement("div")
+ card_action .className="card-actions"// donne la class card a la div cree
+ card.appendChild(card_action)// insere ce div dans la balise qui a pour class .cards-grid appeler container
+ /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+  // cree la div qui  a pour class action delete
+  let supprimer = document.createElement("div")
+  supprimer.className="action delete"//donne la class "action delete" a la div cree
+  supprimer.cloneNode(true) //copie tout ce qui se trouve
+  card_action.appendChild( supprimer) //insere cet div dans la balise qui a pour class "card-actions"
+   /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+  // cree l image qui permmettra de supprimer  
+  const imgdelete = document.createElement("img")
+  imgdelete.src="delete.svg"
+  supprimer.appendChild(imgdelete )//insere cet img dans la balise qui a pour class "action delete""
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+ imgdelete.onclick = () => del(id);
+  // cree la  div  qui  a pour class action update
+  let modifier = document.createElement("div")
+   modifier.className="action update"//donne la class "action update" a la div cree
+modifier.cloneNode(true) //copie tout ce qui se trouve
+card_action .appendChild( modifier) //insere cet div dans la balise qui a pour class "card-actions"
+  /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+  // cree l image qui permmettra de modifier 
+  const updateimage = document.createElement("img")
+  updateimage.src="update.svg"
+  modifier.appendChild(updateimage  )//insere cet img dans la balise qui a pour class "action update""
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+ updateimage.onclick = () => updateNote(id,inputtile,inputext);
+//cree le div++ qui  a pour class "card-wrapper"
+ let wrapper = document.createElement("div")
+ wrapper.className="card-wrapper"// donne la class card-wrapper a la div cree
+ card.appendChild( wrapper)// insere ce div dans la balise qui a pour class .cards-grid appeler container
+ /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+ //cree l identifiant de chaque block
+ let id = document.createElement("span")
+id.innerHTML="#" +i
+ wrapper.appendChild( id)// insere ce bloc dans la balise qui a pour class "card-wrapper" appeler wrapper
 
-    // Mise à jour de l'affichage de la liste
-    afficherListe();
+ 
+ 
+ /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+//cree la balise h1 qui  a pour class "title"
+ let titre = document.createElement("h1")
+titre.className="title"// donne title la classe  au h1 cree
+wrapper.appendChild( titre)// insere ce bloc dans la balise qui a pour class "card-wrapper" wrapper
+titre.innerText=inputtile.value
+inputtile.value= ""
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+// cree la balise p qui  a va contenir la description
+let textarea = document.createElement("p")
+wrapper.appendChild( textarea)// insere ce bloc dans la balise qui a pour class "card-wrapper" wrapper
+textarea.innerText=inputext.value
+inputext.value=""
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+ /* ---------------------------------------------------------------------------------------------------------*/
+ 
+}
+function del(el) {
+    el.parentElement.parentElement.remove(); //supprime le parent et les enfants
+    
+    compt--
+    
+
+    
 }
 
-// Fonction pour afficher la liste d'articles
-function afficherListe() {
-    // Récupération de l'élément de la liste
-    let liste = document.getElementById('liste');
 
-    // Réinitialisation de la liste
-    liste.innerHTML = '';
+function updateNote(el) {
+  save.innerHTML="update"
+  look=1
 
-    // Parcours de la liste d'articles
-    for(let i = 0; i < articles.length; i++) {
-        // Création d'un nouvel élément de liste
-        let element = document.createElement('li');
 
-        // Ajout du titre et de la description à l'élément
-        element.innerHTML = "<h2>" + articles[i].titre + "</h2><p>" + articles[i].description + "</p>";
+    
+  
+  save.onclick = () =>{
+    del(el)
+  
+    if (look==1) {
 
-        // Ajout de l'élément à la liste
-        liste.appendChild(element);
-    }
+ let newtitle =inputtile.value
+ let newtext =inputext.value
+  addCard(i ,inputtile,inputext)
+  look=0
+  save.innerHTML="Enregistrer"
+  
 }
+
+}
+
+}
+
+controlid = function(){
+ 
+
+       
+      
+
+    if (compt=0) {
+        compt++
+
+        return i  
+        
+    }else{
+     i++
+     
+       return i
+  }
+
+}
+
+ 
+save.addEventListener("click",(e) => {
+
+
+e.preventDefault()
+
+    
+
+  alert("entrer un titre et un text")
+
+  
+if (look== 0) {
+  
+
+   controlid()
+    addCard(i ,inputtile,inputext)
+    
+  }})
+  
